589a9ee81ed12e9dbe425d59c7a16a0d3a6876aa
Amaan Ahmed <amaan.ahmed3@gmail.com>
2018-11-09 10:22:56 +0000

    Done.

---
+++ lib/introduction.rb
@@ -0,0 +1,7 @@
+def introduction (name)
+  puts "Hi, my name is #{name}."
+end
+
+def introduction with language (name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
